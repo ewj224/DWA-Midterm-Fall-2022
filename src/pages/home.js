b25eda5475a5d44fcd80af9d3ff58b8e8f8d89a2
ewj224 <enochjo0623@gmail.com>
2022-10-23 19:46:00 -0400

    added new paramters and got steps and time to show up: need to figure out how to loop x amount of steps

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import foodCard from '../components/foodCard';
 import { SPOONACULAR_API_KEY } from '../API_KEYS';
@@ -6,7 +6,7 @@ import { SPOONACULAR_API_KEY } from '../API_KEYS';
 function Home(){
     const [foodData, setFoodData] = useState([]);
     //change pasta eventually to a variable (buttons maybe..?)
-    const URL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=${SPOONACULAR_API_KEY}&instructionsRequired=true&addRecipeInformation=true`
+    const URL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta-and-seafood&apiKey=${SPOONACULAR_API_KEY}&instructionsRequired=true&addRecipeInformation=true`
     useEffect(()=>{
         axios
             .get(URL)
@@ -21,9 +21,24 @@ function Home(){
             });
     }, []);
 
+    const { recipeName, recipeInstructions, recipeTime } = useMemo(()=>{
+        const foodMain = foodData.results && foodData.results[0] || {}
+        const foodMain2 = foodMain.analyzedInstructions && foodMain.analyzedInstructions[0] || {}
+        //steps[0] will be first set of directions
+        const foodMain3 = foodMain2.steps && foodMain2.steps[0] || {}
+        return{
+            recipeName:foodMain.title,
+            recipeInstructions:foodMain3.step,
+            recipeTime: foodMain.readyInMinutes
+               
+        }
+    }, [foodData])
     return (
         <div>
             <h1>Recipes for Food</h1>
+            <h2>{recipeName}</h2>
+            <p>{recipeInstructions}</p>
+            <p>{recipeTime}</p>
             <p>{URL}</p>
         </div>
     )
